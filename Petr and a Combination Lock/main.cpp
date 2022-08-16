156f29f3f59e15cfacbe93c88e5989c0a497a9be
Reinaldo Felipe Hokari Soares Araujo <reinaldo.araujo@mercadolivre.com>
2022-08-15 20:22:33 -0400

    Sumset combinations using bitwise operations

---
+++ Petr and a Combination Lock/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+int main(){
+    int n;
+    std::cin >> n;
+
+    int input[n];
+    for(int i=0;i<n;i++) std::cin >> input[i];
+
+    int totalCombination = 1 << n;
+
+    for(int i=0; i < totalCombination; i++){
+        int sum = 0;
+
+        for(int j=0; j < n; j++){
+            if(i & 1<<j) sum += input[j];
+            else sum-= input[j];
+        }
+
+        if(sum % 360 == 0){
+            std::cout << "YES" << std::endl;
+            return 1;
+        }
+    }
+    std::cout << "NO" << std::endl;
+
+    return 0;
+}
