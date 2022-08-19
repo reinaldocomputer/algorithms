482a3642ec69f5b397d3913b563e330b4d00267b
Reinaldo Felipe Hokari Soares Araujo <reinaldo.araujo@mercadolivre.com>
2022-08-19 16:23:54 -0400

    Dynamic Programming - Fibonacci

---
+++ dp_fibonacci/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#define lli unsigned long long int
+std::vector<lli> fib_vec;
+
+void fibonacci(){
+    lli fib_it = 0;
+    fib_vec.push_back(1);
+    fib_vec.push_back(1);
+
+    while(1){
+        int current = fib_vec.size();
+        fib_it = fib_vec[current-1] + fib_vec[current-2];
+        if(fib_vec[current-2] > INT_FAST64_MAX){
+            break;
+        }
+        fib_vec.push_back(fib_it);
+    }
+}
+
+void print_fibonacci(){
+    std::cout << "Fibonacci Sequence" << std::endl;
+    for(int i=0;i < fib_vec.size();i++) std::cout <<"fib["<<i<<"]:"<< fib_vec[i]<< std::endl;
+}
+int main()
+{
+    fibonacci();
+    print_fibonacci();
+    return 0;
+}
